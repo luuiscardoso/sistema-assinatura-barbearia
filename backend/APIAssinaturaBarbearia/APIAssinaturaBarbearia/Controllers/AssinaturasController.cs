5fa82156365db52c56acb2d0f0d7ecb91cb02934
Luis Cardoso <luiscard.queiroz@gmail.com>
2024-11-03 21:17:36 -0300

    criando metodo de logout

--- backend/APIAssinaturaBarbearia/APIAssinaturaBarbearia/Controllers/AssinaturasController.cs
+++ backend/APIAssinaturaBarbearia/APIAssinaturaBarbearia/Controllers/AssinaturasController.cs
@@ -13,7 +13,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace APIAssinaturaBarbearia.Controllers
 {
-    //[Authorize]
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class AssinaturasController : ControllerBase
